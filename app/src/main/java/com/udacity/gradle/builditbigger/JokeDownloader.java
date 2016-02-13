66f0e868740164ae4f0f47ff38fc53a6fcb7a1d5
iamBedant <bebi2527@live.in>
2016-02-14 00:15:49 +0530

    Deployed backend to google app engine

--- app/src/main/java/com/udacity/gradle/builditbigger/JokeDownloader.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/JokeDownloader.java
@@ -20,6 +20,7 @@ public class JokeDownloader {
     public JokeDownloader(DownloadCompleteListener downloadListener) {
         this.downloadListener = downloadListener;
     }
+
     public void downloadJoke() {
         new EndpointsAsyncTask().execute();
     }
@@ -28,20 +29,8 @@ public class JokeDownloader {
         @Override
         protected String doInBackground(Void... params) {
             if (myApiService == null) {  // Only do this once
-                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                        new AndroidJsonFactory(), null)
-                        // options for running against local devappserver
-                        // - 10.0.2.2 is localhost's IP address in Android emulator
-                        // - turn off compression when running against local devappserver
-                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                            @Override
-                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                                abstractGoogleClientRequest.setDisableGZipContent(true);
-                            }
-                        });
-                // end options for devappserver
-
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                        .setRootUrl("https://[YOUR_PROJECT_ID].appspot.com/_ah/api/");
                 myApiService = builder.build();
             }
             try {
