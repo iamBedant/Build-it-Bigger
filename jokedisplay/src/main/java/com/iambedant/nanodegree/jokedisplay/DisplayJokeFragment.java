15d96f26c0dc8d339c703ca0c88ead76e282b5f1
iamBedant <bebi2527@live.in>
2016-02-13 10:10:27 +0530

    init all modules

---
+++ jokedisplay/src/main/java/com/iambedant/nanodegree/jokedisplay/DisplayJokeFragment.java
@@ -0,0 +1,22 @@
+package com.iambedant.nanodegree.jokedisplay;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class DisplayJokeFragment extends Fragment {
+
+    public DisplayJokeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_display_joke, container, false);
+    }
+}
